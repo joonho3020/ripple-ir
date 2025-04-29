84d463d91434f9d4ccb0ee2787cc3cea2303e31d
maorf08 <maorfuks@gmail.com>
2025-04-28 19:58:24 -0700

    Hash v1.2

--- src/passes/fir/common_subgraph.rs
+++ src/passes/fir/common_subgraph.rs
@@ -66,21 +66,28 @@ pub fn graph_prims(graph: &Graph<u32, u32, Undirected>, start_vertex: NodeIndex)
 // BFS for nodes of exactly distance k away from start node
 pub fn neighbors_of_distance_k(graph: &FirGraph, start: NodeIndex, k: usize) -> Vec<NodeIndex> {
     let mut visited = HashSet::new(); // Dont backtrack
-    let mut vec = Vec::new();
-    vec.push(start);
+    let mut result = Vec::new();
+    let mut current = Vec::new();
+    visited.insert(start);
+    result.push(start);
+    current.push(start);
     for i in 0..k {
-        let mut curr_vec = Vec::new();
-        for neighbor in &vec {
+        let mut next = Vec::new();
+        for neighbor in &current {
             for v in graph.graph.neighbors(*neighbor) {
                 if !visited.contains(&v) {
                     visited.insert(v);
-                    curr_vec.push(v);
+                    result.push(v);
+                    next.push(v);
                 }
             }
         }
-        vec = curr_vec;
+        if next.is_empty() {
+            break;
+        }
+        current = next;
     }
-    return vec
+    return result;
 }
 
 
